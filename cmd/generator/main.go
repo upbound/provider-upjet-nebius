343232e489f9826842a04f97c8f45ee62a4931fd
Jonasz Łasut-Balcerzak <jonasz@upbound.io>
2026-06-10 10:25:24 +0200

    Update github workflows to be compliant with other providers in upbound organization
    Signed-off-by: Jonasz Łasut-Balcerzak <jonasz@upbound.io>

--- cmd/generator/main.go
+++ cmd/generator/main.go
@@ -18,7 +18,7 @@ import (
 
 func main() {
 	var (
-		app                   = kingpin.New("generator", "Run Upjet code generation pipelines for provider-upjet-oci").DefaultEnvars()
+		app                   = kingpin.New("generator", "Run Upjet code generation pipelines for provider-upjet-nebius").DefaultEnvars()
 		repoRoot              = app.Arg("repo-root", "Root directory for the provider repository").Required().String()
 		skippedResourcesCSV   = app.Flag("skipped-resources-csv", "File path where a list of skipped (not-generated) Terraform resource names will be stored as a CSV").Envar("SKIPPED_RESOURCES_CSV").String()
 		generatedResourceList = app.Flag("generated-resource-list", "File path where a list of the generated resources will be stored.").Envar("GENERATED_RESOURCE_LIST").Default("../config/generated.lst").String()
